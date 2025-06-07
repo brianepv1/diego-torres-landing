1af611bd1c86dcf35b0b28695ce12ca879cc5d23
Brian Palomar <brian.epv1@gmail.com>
2025-06-07 16:42:53 -0600

    Change title page

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,8 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: 'Diego Torres - Botanica Torres',
+  description: 'Consultor de tarot y trabajos esotericos en Manzanillo, La Huerta y Mas.',
 }
 
 export default function RootLayout({
